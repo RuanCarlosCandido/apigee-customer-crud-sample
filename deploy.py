4c826c8dcdaa2cdc896723e71b0defb903a393ad
Ruan Candido <84887682+RuanCarlosCandido@users.noreply.github.com>
2023-05-03 19:54:52 -0300

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -117,11 +117,11 @@ def printDeployments(dep):
             print '  Error: %s' % d['error']
 
 ApigeeHost = 'https://api.enterprise.apigee.com'
-UserPW = os.getenv('APIGEE_PASS')
+UserPW = None
 Directory = None
-Organization = os.getenv('APIGEE_ORG')
-Environment = os.getenv('APIGEE_ENV')
-Name = os.getenv('APIGEE_PROXY_NAME')
+Organization = None
+Environment = None
+Name = None
 BasePath = '/'
 ShouldDeploy = True
 
